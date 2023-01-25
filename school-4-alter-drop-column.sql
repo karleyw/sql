653f6442789eca4ab4a14b8a559c6abea9a993e7
karleyw <karley.woods@kellogg.northwestern.edu>
2023-01-25 02:44:02 +0000

    Practicing CRUD

--- school-4-alter-drop-column.sql
+++ school-4-alter-drop-column.sql
@@ -1,2 +1,4 @@
 -- Drop the phone number column
+ALTER TABLE students DROP COLUMN phone;
 
+-- this won't work b/c Gitpod runs an old version of SQLite
---
+++ school-5-insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO students (
+    "first_name",
+    "last_name",
+    "email"
+) VALUES (
+    "Rudy",
+    "Doggo",
+    "bigrudyboy@gmail.com"
+)
+
+-- .mode columns => turns columns on
+-- .headers on => turns column headers on
